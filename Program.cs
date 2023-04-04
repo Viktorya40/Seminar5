31fb7717274deaad74abc40c94d39f35c716bdfa
Viktorya <79201287827@yandex.ru>
2023-04-04 23:44:18 +0300

    Задача 34

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[n];
+
+void array (int n)
+{
+for (int i = 0; i < n; i++)
+{
+randomArray[i] = new Random().Next(99,999);
+Console.Write(randomArray[i] + " ");
+}
+
+}
+
+int count(int[] randomArray)
+{
+int count = 0;
+for (int i = 0; i < randomArray.Length; i++)
+{
+if (randomArray[i] % 2 == 0)
+count = count + 1;
+}
+return count;
+}
+array(n);
+Console.Write($"Количество чётных чисел в массиве: {count(randomArray)}");
